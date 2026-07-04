aaf90b145663a74b8068c3a9b6af746b562535d7
Yasin Moridi <yasinmoridi1384@gmail.com>
2026-07-05 02:53:46 +0330

    push code

---
+++ shared/src/commonMain/kotlin/com/yasinmoridi/miniverse/Greeting.kt
@@ -0,0 +1,9 @@
+package com.yasinmoridi.miniverse
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/yasinmoridi/miniverse/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.yasinmoridi.miniverse
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
