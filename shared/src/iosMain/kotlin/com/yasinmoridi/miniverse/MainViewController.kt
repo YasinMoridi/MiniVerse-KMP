61d94029b6705b75ffe0652081ecee950090e6f6
Yasin Moridi <yasinmoridi1384@gmail.com>
2026-07-06 05:59:15 +0330

    release v1.0.0

--- shared/src/iosMain/kotlin/com/yasinmoridi/miniverse/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/yasinmoridi/miniverse/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.yasinmoridi.miniverse
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.yasinmoridi.miniverse.core.App
 
 fun MainViewController() = ComposeUIViewController { App() }
